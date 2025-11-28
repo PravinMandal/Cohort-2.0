9c925f435af4134c85c42698c9fd2a2645161955
Pravin Mandal <pravinmandalp45@gmail.com>
2025-11-28 13:40:48 +0530

    matrix effect

--- 04. DOM/11. Effects using DOM/02. Random Char/script.js
+++ 04. DOM/11. Effects using DOM/02. Random Char/script.js
@@ -11,18 +11,21 @@ para.addEventListener("mouseenter", () => {
     let i = 0
     const ran = setInterval(() => {
         const str = text.split("").map((char, index) => {
-            if(index < i) {
+            if(index < Math.floor(i)) {
                 return char
             }
-            return charac.split("")[Math.floor(Math.random() * 53)]
+            return charac.split("")[Math.floor(Math.random() * 52)]
         }).join("")
-        para.innerText = str
+        para.innerText = str 
+        console.log(str);
+        
         i += 0.25
+
         if( i > text.length) {
             clearInterval(ran);
             para.innerHTML = text;
             isAnimating = false;
         }
-    }, 30)
+    }, 20)
 
 })
